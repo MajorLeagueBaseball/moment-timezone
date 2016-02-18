d0ef2754f89551b4509d284a77397f3028b0c296
Kyle Baker <kybaker@usatoday.com>
2016-02-18 18:24:45 -0500

    Update moment-timezone-utils.js

--- moment-timezone-utils.js
+++ moment-timezone-utils.js
@@ -9,7 +9,7 @@
 
 	/*global define*/
 	if (typeof define === 'function' && define.amd) {
-		define(['moment'], factory);                 // AMD
+		define(['moment/moment'], factory);                 // AMD
 	} else if (typeof module === 'object' && module.exports) {
 		module.exports = factory(require('./'));     // Node
 	} else {
